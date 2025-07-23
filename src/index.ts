0007d20d0791e0fce863a4abbc0a3ce0dc970b83
MediciX <pzyyxz@gmail.com>
2025-07-23 22:23:18 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import deckRoutes from './routes/decks';
 import cardRoutes from './routes/cards';
 import userRoutes from './routes/users'
 import progressRoutes from './routes/progress';
+import cors from "cors";
 dotenv.config();
 
 connectDB();
@@ -25,3 +26,8 @@ app.use('/api/decks', deckRoutes);
 app.use('/api/cards', cardRoutes);
 app.use('/api/users', userRoutes)
 app.use('/api/progress', progressRoutes);
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
